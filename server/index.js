23ee5e490280ca400de40248f5dc117714ae1530
sbirvin1s <sbirvin1s@gmail.com>
2022-08-09 11:35:19 -0500

    adds initial setup for express server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,26 @@
 /*========== EXTERNAL MODULES ==========*/
+require('dotenv').config();
+const express = require('express');
+
+
+/*========== SYSTEM VARIABLES ==========*/
+const app = express;
+const {PORT} = process.env;
 
 
 /*========== INTERNAL MODULES ==========*/
+const {} = require('./routes');
 
 
-/*========== EXPORTS ==========*/
+/*========== MIDDLEWARE ==========*/
+app.use('/', express.static());
+app.use(express.json());
+
+
+/*========== ROUTES ==========*/
+app.get('/recipes', );
+app.post('/recipes', )
+
+
+/*========== SERVER CONNECTIONS ==========*/
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
