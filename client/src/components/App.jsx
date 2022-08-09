24ffc1dc4485802c8f40e3825cbefac9215ab1af
sbirvin1s <sbirvin1s@gmail.com>
2022-08-09 15:36:00 -0500

    completes bable and webpack configuration to render React components to site

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,29 @@
 /*========== EXTERNAL MODULES ==========*/
-
+import React, {useState} from 'react';
+import axios from 'axios';
 
 /*========== INTERNAL MODULES ==========*/
 
+const App = () =>{
+
+  /*----- STATE HOOKS -----*/
+
+
+  /*----- LIFESTYLE METHODS -----*/
+
+
+  /*----- EVENT HANDLERS -----*/
+
+
+  /*----- RENDER METHODS -----*/
+
+
+  /*----- RENDERER -----*/
+  return (
+    <h1>This is React</h1>
+  )
+}
+
 
 /*========== EXPORTS ==========*/
+export default App;
