440955211cd74ffab0ed63f7890520af41261796
sbirvin1s <sbirvin1s@gmail.com>
2022-11-08 16:28:02 -0600

    adds AddIngredient page to handle seaching and adding ingredient tasks

---
+++ client/src/pages/AddIngredient.jsx
@@ -0,0 +1,29 @@
+/*========== EXTERNAL MODULES ==========*/
+import React from 'react';
+
+/*========== INTERNAL MODULES ==========*/
+import { Input } from '../../dist/styles';
+
+/*========== EXPORTS ==========*/
+export default function AddIngredient({ children, ...props }) {
+
+  /*----- STATE HOOKS -----*/
+
+  /*----- LIFECYCLE METHODS -----*/
+
+  /*----- EVENT HANDLERS -----*/
+
+  /*----- RENDER METHODS -----*/
+
+  /*----- RENDERER -----*/
+  return (
+    <>
+
+    </>
+  )
+}
+
+
+
+
+/*========== STYLES ==========*/
