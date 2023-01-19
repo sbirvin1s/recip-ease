7666973506428ec9bb9c097737d26cb88ffedc12
sbirvin1s <sbirvin1s@gmail.com>
2023-01-18 21:22:15 -0600

    adds inital requirements for user features

--- server/index.js
+++ server/index.js
@@ -9,20 +9,43 @@ const {PORT} = process.env;
 
 
 /*========== INTERNAL MODULES ==========*/
-const {writeRecipe, getRecipes, getIngredient, writeIngredient} = require('./routes/routes');
+const {
+  writeRecipe,
+  getRecipes,
+  getIngredient,
+  writeIngredient
+} = require('./routes/routes');
 
 
 /*========== MIDDLEWARE ==========*/
 app.use('/', express.static(path.join(__dirname,'../client', '/dist')));
 app.use(express.json());
 
-/*TODO: Update add SSR and to use rename current 'Routes' to controllers
- */
+/*TODO: Update add Server Side Rendering(SSR) and to use rename current 'Routes' to controllers*/
+
+/*TODO: #32 User Profile - Server Implementation
+  - [ ] Create API route for client and server communication
+    - [ ] GET route to get all user information
+      - [ ] Current Weight, Fitness Level, Weight Loss goals, and daily caloric goal
+  - [ ] POST / PUT route(s) to:
+    - [ ] Create new account
+    - [ ] Update account information
+    - [ ] Add / Update weight
+    - [ ] Add / Update Fitness Level
+    - [ ] Add / Update weight loss goals
+*/
+
 /*========== ROUTES ==========*/
+/*--- GET ---*/
 app.get('/recipes', getRecipes);
 app.get('/ingredient/:ingredient', getIngredient);
+
+/*--- POST ---*/
 app.post('/ingredient/new', writeIngredient);
 app.post('/recipes', writeRecipe);
+
+/*--- PUT ---*/
+app.put('/user')
 
 
 /*========== SERVER CONNECTIONS ==========*/
