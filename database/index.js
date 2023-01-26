ed445059425e526887d0ac823b9538bf216b6bee
sbirvin1s <sbirvin1s@gmail.com>
2023-01-25 22:05:27 -0600

    slight formatting changes for readability

--- database/index.js
+++ database/index.js
@@ -25,7 +25,7 @@ module.exports = {
    * @param {number} page - page number to return. Default of 1 that adjusts to 0 after logic check
    * @param {number} count - number of items per page. Default of 20
    * @return {promise}
-   */
+  */
   findAllRecipes: (page = 1, count = 20) => {
     let offset = page - 1;
     if (offset < 0) {
@@ -97,8 +97,8 @@ module.exports = {
       return ingredients.forEach(ingredient => {
         [ingredientName, ingredientAmount, ingredientUnit] = ingredient;
         return pool.query('INSERT INTO ingredient_list (recipe_id, ingredient_id, ingredient_amount, ingredient_unit) VALUES (($1), ($2), ($3), ($4))', [recipeId, ingredientName, ingredientAmount, ingredientUnit])
-        })
       })
+    })
     .catch(err => console.error(`Unable to retrieve ingredient due to ${err}`))
   },
 
@@ -158,23 +158,22 @@ module.exports = {
         totalCarbs, fiber, sugar,
         protein, vitaminA, vitaminC, vitaminD,
         calcium, iron, potassium
-      } = newIngredient;
+    } = newIngredient;
 
-      return pool.query(
-        'INSERT INTO ingredients ( \
-          ingredient, brand, food_category, upc, serving_size, serving_unit, servings_per_container, calories, total_fat, sat_fat, trans_fat, poly_fat, mono_fat, cholesterol, sodium, total_carbs, fiber, sugar, protein, vitamin_a, vitamin_c, vitamin_d, calcium, iron, potassium \
-        ) \
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25) \
-        RETURNING *;', [
-        ingredientName, brand, foodCategory,
-        barcode, servingSize, servingUnit,
-        servingPerContainer, calories, totalFat,
-        satFat, transFat, polyUnSatFat,
-        monoUnSatFat, cholesterol, sodium,
-        totalCarbs, fiber, sugar,
-        protein, vitaminA, vitaminC, vitaminD,
-        calcium, iron, potassium
-      ])
+    return pool.query(
+      'INSERT INTO ingredients ( \
+        ingredient, brand, food_category, upc, serving_size, serving_unit, servings_per_container, calories, total_fat, sat_fat, trans_fat, poly_fat, mono_fat, cholesterol, sodium, total_carbs, fiber, sugar, protein, vitamin_a, vitamin_c, vitamin_d, calcium, iron, potassium \
+      ) \
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25) \
+      RETURNING *;', [
+      ingredientName, brand, foodCategory,
+      barcode, servingSize, servingUnit,
+      servingPerContainer, calories, totalFat,
+      satFat, transFat, polyUnSatFat,
+      monoUnSatFat, cholesterol, sodium,
+      totalCarbs, fiber, sugar,
+      protein, vitaminA, vitaminC, vitaminD,
+      calcium, iron, potassium
+    ])
   },
-
 }
